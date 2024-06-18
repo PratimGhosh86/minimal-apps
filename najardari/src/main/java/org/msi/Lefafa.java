9a0147a63c7c837fad36b7711fc18d5727b75247
Pratim Ghosh <17795322+PratimGhosh86@users.noreply.github.com>
2024-06-19 04:11:53 +0530

    minor changes

--- najardari/src/main/java/org/msi/Lefafa.java
+++ najardari/src/main/java/org/msi/Lefafa.java
@@ -6,14 +6,19 @@ import java.io.IOException;
 
 public class Lefafa {
 
-	public static void main(final String[] args) throws IOException {
-		final Console sadaKaloJanla = System.console();
-		if (sadaKaloJanla == null && !GraphicsEnvironment.isHeadless()) {
-			final String naam = Lefafa.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6)
-					.replace("%20", " ");
-			Runtime.getRuntime().exec(new String[] { "cmd", "/c", "start", "cmd", "/k", "java -jar \"" + naam + "\"" });
-		} else
-			Najardari.main(new String[0]);
-	}
-
+  public static void main(final String[] args) throws IOException {
+    final Console sadaKaloJanla = System.console();
+    if (sadaKaloJanla == null && !GraphicsEnvironment.isHeadless()) {
+      final String naam =
+          Lefafa.class
+              .getProtectionDomain()
+              .getCodeSource()
+              .getLocation()
+              .toString()
+              .substring(6)
+              .replace("%20", " ");
+      Runtime.getRuntime()
+          .exec(new String[] {"cmd", "/c", "start", "cmd", "/k", "java -jar \"" + naam + "\""});
+    } else Najardari.main(new String[0]);
+  }
 }
